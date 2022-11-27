3933ee51d8df820f3fa75dfcbc05633bbc423960
Hyun <itsmine096@gmail.com>
2022-11-27 20:25:08 +0900

    create Spring Project

---
+++ JPA_prac/src/main/java/com/hyun/jpa_prac/JpaPracApplication.java
@@ -0,0 +1,13 @@
+package com.hyun.jpa_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaPracApplication.class, args);
+    }
+
+}
---
+++ JPA_prac/src/test/java/com/hyun/jpa_prac/JpaPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyun.jpa_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
